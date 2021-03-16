9260eadd09080c604be42c9dc8a7a91eb764d1ef
alexander193 <sana2001@list.ru>
2021-03-16 13:44:16 +0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -307,6 +307,7 @@ $('.mbtn').click(function () {
 $(document).ready(function() {
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
    
+  alert("Вы зашли с мобилы");
     $('.text').css('font-size') = "217px";
   }
   else
