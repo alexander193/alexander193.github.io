2d9dc2f316cd03d34801f35ea777b83185f6b5a0
alexander193 <sana2001@list.ru>
2021-03-16 13:49:23 +0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -305,6 +305,7 @@ $('.mbtn').click(function () {
 });
 
 $(document).ready(function() {
+  let detect = new MobileDetect(window.navigator.userAgent)
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
    
   alert("Вы зашли с мобилы");
