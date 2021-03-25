ef47be9e2d7c26082c6fb4f9f83dafb4f75e105d
alexander193 <sana2001@list.ru>
2021-03-25 22:25:23 +0300

    fixes

--- services/main.js
+++ services/main.js
@@ -1,6 +1,6 @@
 
 const panoImage = document.querySelector('.pano-image');
-const housePano = '../images/house.jpg';
+const housePano = '../images/house.jpeg';
 const panorama = new PANOLENS.ImagePanorama(housePano);
 const viewer = new PANOLENS.Viewer({
     container: panoImage
