d1c53f4718264d6f9bb03cad5775a8138b738d20
alexander193 <sana2001@list.ru>
2021-03-26 15:09:30 +0300

    Update main.js

--- services/main.js
+++ services/main.js
@@ -1,9 +1,8 @@
 
 const panoImage = document.querySelector('.pano-image');
-const housePano = '../images/hous2e.jpeg';
+const housePano = '../images/house2.jpeg';
 const panorama = new PANOLENS.ImagePanorama(housePano);
 
-const path = '../images/point.png';
 infospot1 = new PANOLENS.Infospot(1350, path, false);
 infospot1.position.set(-2787.53, -794.60, 4067.16);
 infospot1.addHoverText('Mountain Tip');
