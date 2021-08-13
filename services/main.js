e869914852aeb7d96187c6a9b44184fc78ab80a0
alexander193 <sana2001@list.ru>
2021-08-13 18:11:59 +0300

    test_progressbar

--- services/main.js
+++ services/main.js
@@ -1,9 +1,33 @@
-var panorama, panorama2, viewer, container, infospot;
+var panorama, panorama2, viewer, container, infospot, progress, progressElement;
+
+progressElement = document.getElementById( 'progress' );
+
+function onEnter ( event ) {
+
+  progressElement.style.width = 0;
+  progressElement.classList.remove( 'finish' );
+
+}
+
+function onProgress ( event ) {
+
+  progress = event.progress.loaded / event.progress.total * 100;
+  progressElement.style.width = progress + '%';
+  if ( progress === 100 ) {
+    progressElement.classList.add( 'finish' );
+  }
+
+}
 
 container = document.querySelector( '.pano-image' );
 
 panorama = new PANOLENS.ImagePanorama( '../images/house.jpeg' );
+panorama.addEventListener( 'progress', onProgress );
+panorama.addEventListener( 'enter', onEnter );
+
 panorama2 = new PANOLENS.ImagePanorama( '../images/house2.jpeg' );
+panorama2.addEventListener( 'progress', onProgress );
+panorama2.addEventListener( 'enter', onEnter );
 
 infospot = new PANOLENS.Infospot(1000, '../images/strelka.png');
 infospot.position.set(3376.66, -2773.43, -2420.08);
