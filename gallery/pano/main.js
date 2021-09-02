127b6283b3536aaf5677fa078e23736ac554b2fb
Alexander <sana2001@list.ru>
2021-09-02 17:45:16 +0300

    Update main.js

--- gallery/pano/main.js
+++ gallery/pano/main.js
@@ -1,34 +1,34 @@
 var panorama, panorama2, viewer, container, infospot, progress, progressElement, table;
 
-table = document.getElementById( 'table_prog' );
-progressElement = document.getElementById( 'progress' );
+table = document.getElementById('table_prog');
+progressElement = document.getElementById('progress');
 
-function onEnter ( event ) {
+function onEnter(event) {
 
-  progressElement.classList.remove( 'finish' );
+  progressElement.classList.remove('finish');
 
 }
 
-function onProgress ( event ) {
+function onProgress(event) {
   progress = event.progress.loaded / event.progress.total * 100;
   progressElement.textContent = Math.round(progress) + '%';
-  
-  if ( progress === 100 ) {
-    progressElement.classList.add( 'finish' );
+
+  if (progress === 100) {
+    progressElement.classList.add('finish');
     table.style.display = 'none';
   }
 
 }
 
-container = document.querySelector( '.pano-image' );
+container = document.querySelector('.pano-image');
 
-panorama = new PANOLENS.ImagePanorama( '../../images/house.jpeg' );
-panorama.addEventListener( 'progress', onProgress );
-panorama.addEventListener( 'enter', onEnter );
+panorama = new PANOLENS.ImagePanorama('../../images/house.jpeg');
+panorama.addEventListener('progress', onProgress);
+panorama.addEventListener('enter', onEnter);
 
-panorama2 = new PANOLENS.ImagePanorama( '../../images/house2.jpeg' );
-panorama2.addEventListener( 'progress', onProgress );
-panorama2.addEventListener( 'enter', onEnter );
+panorama2 = new PANOLENS.ImagePanorama('../../images/house2.jpeg');
+panorama2.addEventListener('progress', onProgress);
+panorama2.addEventListener('enter', onEnter);
 
 // infospot = new PANOLENS.Infospot(1000, '../../images/strelka.png');
 // infospot.position.set(3376.66, -2773.43, -2420.08);
@@ -45,11 +45,17 @@ panorama2.addEventListener( 'enter', onEnter );
 // } );
 
 // panorama.add( infospot );
-viewer = new PANOLENS.Viewer();
+viewer = new PANOLENS.Viewer({
+  container: container,
+  cameraFov: 75,
+  autoHideInfospot: true,
+  controlBar: true,
+  output: 'console'
+});
 viewer.OrbitControls.noZoom = true;
 viewer.add(panorama, panorama2);
 
 
-panorama.addEventListener( 'enter-fade-start', function() {
-  viewer.tweenControlCenter( new THREE.Vector3(2606.45, -4157.60, 930.37), 0 );
+panorama.addEventListener('enter-fade-start', function () {
+  viewer.tweenControlCenter(new THREE.Vector3(2606.45, -4157.60, 930.37), 0);
 });
