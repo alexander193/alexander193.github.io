775c8192265200ba358337188fc9e9398d017439
Alexander <sana2001@list.ru>
2021-09-02 17:54:46 +0300

    Update main.js

--- gallery/pano/main.js
+++ gallery/pano/main.js
@@ -20,7 +20,7 @@ function onProgress(event) {
 
 }
 
-// container = document.querySelector('.pano-image');
+container = document.querySelector('.pano-image');
 
 panorama = new PANOLENS.ImagePanorama('../../images/house.jpeg');
 panorama.addEventListener('progress', onProgress);
@@ -45,7 +45,14 @@ panorama2.addEventListener('enter', onEnter);
 // } );
 
 // panorama.add( infospot );
-viewer = new PANOLENS.Viewer();
+viewer = new PANOLENS.Viewer({
+  container: container,
+  cameraFov: 75,
+  autoHideInfospot: true,
+  controlBar: true,
+  output: 'console'
+});
+viewer.OrbitControls.noZoom = true;
 viewer.add(panorama, panorama2);
 
 
