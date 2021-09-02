823e456289abd3d768ae12ff10e8cf16e3b1c409
Alexander <sana2001@list.ru>
2021-09-02 17:17:01 +0300

    test

--- gallery/pano/main.js
+++ gallery/pano/main.js
@@ -30,27 +30,32 @@ panorama2 = new PANOLENS.ImagePanorama( '../../images/house2.jpeg' );
 panorama2.addEventListener( 'progress', onProgress );
 panorama2.addEventListener( 'enter', onEnter );
 
-//infospot = new PANOLENS.Infospot(1000, '../../images/strelka.png');
-//infospot.position.set(3376.66, -2773.43, -2420.08);
-//infospot.addEventListener( 'click', function(){
-  //viewer.setPanorama( panorama2 );
-  //infospot2.position.set(-2854.75, -2548.67, -3212.39);
-  //panorama2.add( infospot2 );
-//} );
+infospot = new PANOLENS.Infospot(1000, '../../images/strelka.png');
+infospot.position.set(3376.66, -2773.43, -2420.08);
+infospot.addEventListener( 'click', function(){
+  viewer.setPanorama( panorama2 );
+  infospot2.position.set(-2854.75, -2548.67, -3212.39);
+  panorama2.add( infospot2 );
+} );
 
-//infospot2 = new PANOLENS.Infospot(1000, '../../images/strelka2.png');
-//infospot2.addEventListener( 'click', function(){
-  //viewer.setPanorama( panorama );
-  //panorama.add( infospot );
-//} );
+infospot2 = new PANOLENS.Infospot(1000, '../../images/strelka2.png');
+infospot2.addEventListener( 'click', function(){
+  viewer.setPanorama( panorama );
+  panorama.add( infospot );
+} );
 
-//panorama.add( infospot );
-viewer = new PANOLENS.Viewer( {
+panorama.add( infospot );
+viewer = new PANOLENS.Viewer( { 
+    container: container,
+    cameraFov: 75,
+    autoHideInfospot: true,
+    controlBar: true,
     output: 'console'
  } );
 viewer.add(panorama, panorama2);
 viewer.OrbitControls.noZoom = true;
-
-panorama.addEventListener( 'enter-fade-start', function() {
-  viewer.tweenControlCenter( new THREE.Vector3(2606.45, -4157.60, 930.37), 0 );
+//viewer.autoHideInfospot = false;
+viewer.camera.position.set(2606.45, -4157.60, 930.37);
+viewer.addUpdateCallback(function(){
+  
 });
