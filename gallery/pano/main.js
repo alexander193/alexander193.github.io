a55d78e9fdabeaf0f992cddbe0dbfb3543d2f679
alexander193 <sana2001@list.ru>
2021-09-03 14:36:26 +0300

    test

--- gallery/pano/main.js
+++ gallery/pano/main.js
@@ -1,55 +1,65 @@
-var panorama, panorama2, viewer, container, infospot, progress, progressElement, table;
+const panorama = new PANOLENS.ImagePanorama('../../images/house.jpeg');
+const viewer = new PANOLENS.Viewer( { output: 'console' } );
+viewer.add( panorama );
 
-table = document.getElementById('table_prog');
-progressElement = document.getElementById('progress');
+// var panorama, panorama2, viewer, container, infospot, progress, progressElement, table;
 
-function onEnter(event) {
+// table = document.getElementById('table_prog');
+// progressElement = document.getElementById('progress');
 
-  progressElement.classList.remove('finish');
+// function onEnter(event) {
 
-}
+//   progressElement.classList.remove('finish');
 
-function onProgress(event) {
-  progress = event.progress.loaded / event.progress.total * 100;
-  progressElement.textContent = Math.round(progress) + '%';
+// }
 
-  if (progress === 100) {
-    progressElement.classList.add('finish');
-    table.style.display = 'none';
-  }
+// function onProgress(event) {
+//   progress = event.progress.loaded / event.progress.total * 100;
+//   progressElement.textContent = Math.round(progress) + '%';
 
-}
+//   if (progress === 100) {
+//     progressElement.classList.add('finish');
+//     table.style.display = 'none';
+//   }
 
-container = document.querySelector('.pano-image');
+// }
 
-panorama = new PANOLENS.ImagePanorama('../../images/house.jpeg');
-panorama.addEventListener('progress', onProgress);
-panorama.addEventListener('enter', onEnter);
+// container = document.querySelector('.pano-image');
 
-panorama2 = new PANOLENS.ImagePanorama('../../images/house2.jpeg');
-panorama2.addEventListener('progress', onProgress);
-panorama2.addEventListener('enter', onEnter);
+// panorama = new PANOLENS.ImagePanorama('../../images/house.jpeg');
+// panorama.addEventListener('progress', onProgress);
+// panorama.addEventListener('enter', onEnter);
 
-// infospot = new PANOLENS.Infospot(1000, '../../images/strelka.png');
-// infospot.position.set(3376.66, -2773.43, -2420.08);
-// infospot.addEventListener( 'click', function(){
-//   viewer.setPanorama( panorama2 );
-//   infospot2.position.set(-2854.75, -2548.67, -3212.39);
-//   panorama2.add( infospot2 );
-// } );
+// panorama2 = new PANOLENS.ImagePanorama('../../images/house2.jpeg');
+// panorama2.addEventListener('progress', onProgress);
+// panorama2.addEventListener('enter', onEnter);
 
-// infospot2 = new PANOLENS.Infospot(1000, '../../images/strelka2.png');
-// infospot2.addEventListener( 'click', function(){
-//   viewer.setPanorama( panorama );
-//   panorama.add( infospot );
-// } );
+// // infospot = new PANOLENS.Infospot(1000, '../../images/strelka.png');
+// // infospot.position.set(3376.66, -2773.43, -2420.08);
+// // infospot.addEventListener( 'click', function(){
+// //   viewer.setPanorama( panorama2 );
+// //   infospot2.position.set(-2854.75, -2548.67, -3212.39);
+// //   panorama2.add( infospot2 );
+// // } );
 
-// panorama.add( infospot );
-viewer = new PANOLENS.Viewer();
-viewer.OrbitControls.noZoom = true;
-viewer.add(panorama, panorama2);
+// // infospot2 = new PANOLENS.Infospot(1000, '../../images/strelka2.png');
+// // infospot2.addEventListener( 'click', function(){
+// //   viewer.setPanorama( panorama );
+// //   panorama.add( infospot );
+// // } );
+
+// // panorama.add( infospot );
+// viewer = new PANOLENS.Viewer({
+//   container: container,
+//   cameraFov: 75,
+//   autoHideInfospot: true,
+//   controlBar: true,
+//   output: 'console'
+// });
+// viewer.OrbitControls.noZoom = true;
+// viewer.add(panorama, panorama2);
 
 
-panorama.addEventListener('enter-fade-start', function () {
-  viewer.tweenControlCenter(new THREE.Vector3(2606.45, -4157.60, 930.37), 0);
-});
+// panorama.addEventListener('enter-fade-start', function () {
+//   viewer.tweenControlCenter(new THREE.Vector3(2606.45, -4157.60, 930.37), 0);
+// });
