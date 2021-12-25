b0dff5796c22e75661cfd69b25f5848590abc4f3
Alexander <sana2001@list.ru>
2021-12-26 01:09:27 +0300

    Update main.js

--- gallery/pano/main.js
+++ gallery/pano/main.js
@@ -35,8 +35,8 @@ progressElement = document.getElementById('progress');
 credit = document.getElementById('credit');
 divvv = document.getElementById('divvv');
 
-progressElement.style.paddingBottom = '50vh' - '60px';
-progressElement.style.paddingTop = '50vh' - '60px';
+progressElement.style.paddingBottom = '50vh';
+progressElement.style.paddingTop = '50vh';
 
 function onEnter(event) {
   progressElement.classList.remove('finish');
